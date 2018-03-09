ef200959d724e4e8dda0ed87b74b0021b35eb102
Arek <kotecki.arkadiusz@gmail.com>
2018-03-09 11:55:44 +0100

    Update categoryfull.php

--- categoryfull.php
+++ categoryfull.php
@@ -2,130 +2,91 @@
 
 <main class="container" role="main" id="main">
     <div class="row">
-      <div class="col-sm-12 col-md-8 col-md-push-2">
-        <div class="row">
-			<?php
-				$subkategorie = array();
-				if (is_category()){
-					$cur_cat = get_query_var('cat');
-					
-					if ($cur_cat){
-						$subkategorie = get_categories(array(
-							'parent' => $cur_cat,
-							'depth' => 1,
-							'orderby'       => 'name',
-							'order'         => 'ASC',
-							'hierarchical'  => true,
-							'pad_counts' => 0
-						));
-					}
-					
-					if (sizeof($subkategorie)>0){
-						echo '<h3>subkategorie</h3><ul>';
-						foreach($subkategorie as $kategoria){
-							echo '<li><a href="'.get_term_link($kategoria).'">'.$kategoria->name.'</a></li>';
-						}
-						echo '</ul>';
-					}
-					else {
-						echo '<h3>posty '.$cur_cat.'</h3>';
-						$paged = (get_query_var('paged') === 0)? 1 : get_query_var('paged');
-						print_r($paged);
-						$myposts = new WP_Query(array('posts_per_page' => 100, 'paged' => $paged, 'cat' => $cur_cat, 'post_type' => 'any', 'post_status'=>'publish'));
-						while($myposts->have_posts()): $myposts->the_post(); ?> 
+		<div class="col-sm-12 col-md-8 col-md-push-2">
+			<div class="row">
+				<?php
+					$subkategorie = array();
+					if (is_category()){
+						$cur_cat = get_query_var('cat');
 						
-						<h2><?php the_title();?></h2>
-						<a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php the_title();?></a>
-												
-						<?php 
-							 //print_r($post);
-						endwhile;?>
- 
-						<?php
-						wp_pagenavi( array( 'query' => $myposts ) );
-						wp_reset_postdata();
-					}
-					
-					
-				}
-				
-			?>	
-			<section class="col-xs-6 custom-xs-12 singleAktualnosc">
-				<div class="wiadomoscSingleListWrapperImg">
-				
-					<img src="" alt="">
-		
-					<img src="" alt="">
-			
-				</div>
-				<div class="singleAktualnoscInfoBox">
-					<time datetime="" class="singleAktualnoscInfoBoxTime"></time>
-					<h2 class="singleAktualnoscInfoBoxTitle"></h2>
-					<p class="singleAktualnoscInfoBoxExcerpt"></p>
-					<a href="" class="singleAktualnoscInfoBoxMore">więcej &gt;</a>
-				</div>
-			 </section>
-			 
-			<div class="col-xs-12 paginationWrapper"></div>
+						if ($cur_cat){
+							$subkategorie = get_categories(array(
+								'parent' => $cur_cat,
+								'depth' => 1,
+								'orderby'       => 'name',
+								'paged' 		=> $paged,
+								'order'         => 'ASC',
+								'hierarchical'  => true,
+								'pad_counts' => 0
+							));
+						}
+						
+						if (sizeof($subkategorie)>0){
+							foreach($subkategorie as $kategoria){
+								?>
+								
+								<article class="col-xs-6 custom-xs-12 singleAktualnosc">
+									<div class="singleElementPodzial">
+										<h3 class="singleAktualnoscInfoBoxTitle"><?php echo $kategoria->name;?></h3>
+										<a href="<?php echo get_term_link($kategoria); ?>" class="singleAktualnoscInfoBoxMore" title="<?php echo $kategoria->name;?>">więcej &gt;</a>
+									</div>
+								</article>
 
+								<?php
+								
+							}
+						} 
+						else {
+							
+							$paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1; $args = array( 'posts_per_page' => get_option('posts_per_page'), 'paged' => $paged, );
+							$myposts = new WP_Query(
+									array(
+									'posts_per_page' => 20, 
+									'paged' => $paged, 
+									'cat' => $cur_cat, 
+									'post_type' => 'any', 
+									'post_status'=>'publish',
+									'total' => $wp_query->max_num_pages
+									));
+								while($myposts->have_posts()): $myposts->the_post(); ?> 
+								<article class="col-xs-6 custom-xs-12 singleAktualnosc singleAktualnoscHeight">
+									<div class="wiadomoscSingleListWrapperImg">
+										<?php if(has_post_thumbnail()){ ?>
+											<img src="<?php the_post_thumbnail_url(); ?>" alt="<?php echo $imgAlt; ?>">
+										<?php }else{ ?>
+											<img src="<?php echo get_template_directory_uri(); ?>/images/logo_zastepcze.png" alt="Obraz zastępczy">
+										<?php } ?>
+									</div>
+									<div class="singleElementPodzial">
+										<h3 class="singleAktualnoscInfoBoxTitle"><?php the_title();?></h3>
+										<p class="singleAktualnoscInfoBoxExcerpt"><?php echo mb_substr(($post -> post_content),0 ,240); ?></p>
+										<a href="<?php the_permalink();?>" class="singleAktualnoscInfoBoxMore displayBlock">więcej &gt;</a>
+									</div>
+								</article> 
+																				
+								<?php 
+								endwhile;?>
+	 
+							<?php						
+								wp_pagenavi( array( 'query' => $myposts ) );
+								wp_reset_postdata();	
+						}
+					}
+				?>				 
+				<div class="col-xs-12 paginationWrapper"></div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-2 col-md-pull-8 aktualnosciSidebar">
+			<?php dynamic_sidebar( 'aktualnosc_left' ); ?>
+		</div>
+		<div class="col-sm-6 col-md-2 aktualnosciSidebarRight">
+			<div class="archiveBox">
+			  <a href="#">Archiwum<br>Wiadomości</a>
+			</div>
+			<div class="row">
+			  <?php dynamic_sidebar( 'aktualnosc_right' ); ?>
+			</div>
         </div>
-      </div>
-      <div class="col-sm-6 col-md-2 col-md-pull-8 aktualnosciSidebar">
-        <h3 class="mainTitle">Województwo</h3>
-        <ul class="aktualnosciSidebarUl">
-          <li><a href="">Lorem ipsum</a></li>
-          <li><a href="">Lorem ipsum</a></li>
-          <li class="active"><a href="">Lorem ipsum</a></li>
-          <li><a href="">Lorem ipsum</a></li>
-        </ul>
-        <h3 class="mainTitle">Filtruj</h3>
-          <form action="" class="filterForm">
-		  <fieldset>
-			<legend class="hidden">Filtrowanie</legend>
-			<label for="sejmik" class="hidden">Sejmik</label>
-              <p><input type="checkbox" name="vehicle" id="sejmik" value="jeden"> Sejmik</p>
-			  <label for="Urzad" class="hidden">Sejmik</label>
-              <p><input type="checkbox" name="vehicle" id="Urzad" value="dwa" checked="checked"> Urząd</p>
-			  <label for="Zarzad" class="hidden">Sejmik</label>
-              <p><input type="checkbox" name="vehicle" id="Zarzad" value="trzy"> Zarząd</p>
-			  <label for="sejmik1" class="hidden">Sejmik</label>
-              <p><input type="checkbox" name="vehicle" id="sejmik1" value="cztery"> Sejmik</p>
-			  <label for="Urzad2" class="hidden">Sejmik</label>
-              <p><input type="checkbox" name="vehicle" id="Urzad2" value="piec" checked="checked"> Urząd</p>
-			  <label for="Zarzad3" class="hidden">Sejmik</label>
-              <p><input type="checkbox" name="vehicle" id="Zarzad3" value="szejsc"> Zarząd</p>
-              <input type="submit" value="SZUKAJ">
-			  </fieldset>
-          </form>
-      </div>
-      <div class="col-sm-6 col-md-2 aktualnosciSidebarRight">
-        <div class="archiveBox">
-          <a href="#">Archiwum<br>Wiadomości</a>
-        </div>
-        <div class="row">
-          <div class="col-xs-6 col-sm-12">
-            <div class="archiveBoxSingle">
-              <div>
-                <p>Lorem Ipsum Dolor</p>
-              <span>Sit amet snbsbdkjnfsdk  kj bkj  njdskfnsjdkf njsnjk fnsd jkdsn kjnsdskjd fnso iio</span>
-              </div>
-            </div>
-          
-          
-        </div>
-        <div class="col-xs-6 col-sm-12">
-          <div class="archiveBoxSingle">
-            <img src="./images/aktualnosciArchive.jpg" alt="">
-            <div>
-              <p>Lorem Ipsum Dolor</p>
-            <span>Sit amet snbsbdkjnfsdk  kj bkj  njdskfnsjdkf njsnjk fnsd jkdsn kjnsdskjd fnso iio</span>
-            </div>
-          </div>
-        </div>
-          
-          
-        </div>
-        
       </div>
     </div>
   </main>
